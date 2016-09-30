6255d2aa21ef1fbd5193f6dede058bd842d3ba03
shubham715 <shubhamsharma715@gmail.com>
2016-09-30 09:58:46 +0530

    first commit

---
+++ phpframe/config/config.php
@@ -0,0 +1,11 @@
+
+<?php
+ 
+/** Configuration Variables **/
+ 
+define ('DEVELOPMENT_ENVIRONMENT',true);
+ 
+define('DB_NAME', 'forms');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
